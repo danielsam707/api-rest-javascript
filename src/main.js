4f8834655a7873ace25d467841065278fe07aece
Daniel Arias <danielsam707@gmail.com>
2024-12-18 08:54:10 -0500

    Clase 6: Lista de categorias

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+// FUNCION: OBTENER PELICULAS RECOMENDADAS POR DIA
 async function getTrendingMoviesPreview() {
     const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
     const data = await res.json();
@@ -24,8 +26,39 @@ async function getTrendingMoviesPreview() {
         movieContainer.appendChild(movieImg);
         trendingPreviewMoviesContainer.appendChild(movieContainer);
     })
-
-
 }
 
+
+//FUNCION: OBTENER CATEGORIAS
+async function getCategoriesPreview() {
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const data = await res.json();
+
+    const categories = data.genres;
+    console.log(categories)
+    
+    //RECORRER LAS PELICULAS
+    categories.forEach((category) => {
+
+        //Traigo el contenedor article
+        const previewCategoiresContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+        //Creo un div contenedor
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        //Creo el elemnto title
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id'+category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        //Agregar los elementos al HTML
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoiresContainer.appendChild(categoryContainer);
+    })
+}
+
+getCategoriesPreview();
 getTrendingMoviesPreview();
